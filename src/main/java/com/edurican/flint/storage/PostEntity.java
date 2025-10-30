1db7b69568e186e6495666f1ecde9b788dc0648f
ChoiKYH <priyune@naver.com>
2025-10-30 18:51:53 +0900

    source update

--- src/main/java/com/edurican/flint/storage/PostEntity.java
+++ src/main/java/com/edurican/flint/storage/PostEntity.java
@@ -17,13 +17,11 @@ public class PostEntity extends BaseEntity{
     @Column(name = "content", length = 100)
     private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false )
-    @JoinColumn(name = "user_id")
-    private UserEntity user;
+    @Column(name = "user_id" )
+    private Long user;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = true)
-    @JoinColumn(name = "topic_id")
-    private TopicEntity topic;
+    @Column(name = "topic_id", nullable=true)
+    private Long topic;
 
     @Column(name = "view_count")
     private Integer viewCount;
@@ -34,9 +32,7 @@ public class PostEntity extends BaseEntity{
     @Column(name = "respark_count")
     private Integer resparkCount;
 
-    //status로 하는거라서 삭제해도 될듯????
-    @Column(name = "is_delete" , nullable = false)
-    private boolean isDelete;
+
 
 
 }
