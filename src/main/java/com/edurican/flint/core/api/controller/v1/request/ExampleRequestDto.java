c1590da9d4a6085bfd4d0877bc66607260304112
ucb1122 <ucb1122@naver.com>
2025-10-30 10:02:50 +0900

    add. comment
    - comment

--- src/main/java/com/edurican/flint/core/api/controller/v1/request/ExampleRequestDto.java
+++ src/main/java/com/edurican/flint/core/api/controller/v1/request/ExampleRequestDto.java
@@ -1,6 +1,7 @@
 package com.edurican.flint.core.api.controller.v1.request;
 
 import com.edurican.flint.core.domain.ExampleData;
+import lombok.Data;
 
 public record ExampleRequestDto(String data) {
     public ExampleData toExampleData() {
