007e9648be586840354fc106364f4f53026c6ff3
haengguk <guddnrb14@naver.com>
2025-10-30 19:43:53 +0900

    added User Domain, modified UserEntity extends
    - 유저 도메인 추가
    - 유저 엔티티 상속 변경

---
+++ src/main/java/com/edurican/flint/core/domain/User.java
@@ -0,0 +1,25 @@
+package com.edurican.flint.core.domain;
+
+import com.edurican.flint.storage.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String bio;
+
+    private Integer followersCount;
+
+    private Integer followingCount;
+
+}
