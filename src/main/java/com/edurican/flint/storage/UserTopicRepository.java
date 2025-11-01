80fddd539f2e653c023bc185419cf657c9b6abf5
Apeirogon99 <apeirogon99@gmail.com>
2025-11-01 18:39:26 +0900

    add user topic entity
    - user topic entity 추가
    - user topic repoistory 추가, score 증가

---
+++ src/main/java/com/edurican/flint/storage/UserTopicRepository.java
@@ -0,0 +1,15 @@
+package com.edurican.flint.storage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserTopicRepository extends JpaRepository<UserTopicEntity, Long> {
+
+    UserTopicEntity findByUserIdAndTopicId(Long userId, Long topicId);
+
+    @Modifying
+    @Query("UPDATE UserTopicEntity ut SET ut.score = ut.score + 1 WHERE ut.userId = :userId AND ut.topicId = :topicId")
+    int incrementScore(@Param("userId") Long  userId, @Param("topicId") Long topicId);
+}
