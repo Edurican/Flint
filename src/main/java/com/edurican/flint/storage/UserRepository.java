e960ae56c851fb96fe7661fde7eae3e7811ecf29
haengguk <guddnrb14@naver.com>
2025-11-07 18:58:41 +0900

    - added UserProfileResponse, modified UserService, UserControlle
    - 유저 프로필 뷰 연동을 위한 Response 계층 클래스 생성
    - 유저 프로필 조회 GetMapping 메소드 추가, 유저 이름 기반 프로필 리턴 메소드 추가

--- src/main/java/com/edurican/flint/storage/UserRepository.java
+++ src/main/java/com/edurican/flint/storage/UserRepository.java
@@ -1,10 +1,20 @@
 package com.edurican.flint.storage;
 
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
+    @Query("SELECT p FROM PostEntity p WHERE p.id < :cursor ORDER BY p.id DESC LIMIT :limit")
+    Slice<PostEntity> findAllWithCursor(@Param("cursor") Long cursor, Integer limit);
+
+    @Query("SELECT p FROM PostEntity p WHERE p.userId = :userId AND p.id < :cursor ORDER BY p.id DESC LIMIT :limit")
+    Slice<PostEntity> findByUserIdWithCursor(@Param("userId") Long userId, @Param("cursor") Long cursor, Integer limit);
+
+
 }
