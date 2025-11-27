58887ddf234387f10e4a2082a6d0a67e17d11f58
ucb1122 <ucb1122@naver.com>
2025-11-28 01:59:46 +0900

    refactor(comment): jpa entity 수정

--- src/main/java/com/edurican/flint/storage/CommentLikeRepository.java
+++ src/main/java/com/edurican/flint/storage/CommentLikeRepository.java
@@ -3,16 +3,8 @@ package com.edurican.flint.storage;
 
 import com.edurican.flint.core.domain.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long>, CommentLikeRepositoryCustom {
 
     int deleteByUserIdAndCommentId(Long userId, Long commentId);
-
-    boolean existsByUserIdAndCommentId(Long userId, Long commentId);
-
-    @Query("select count(cl) from CommentLike cl where cl.commentId = :commentId")
-    Integer countByCommentId(@Param("commentId") Long commentId);
-
 }
