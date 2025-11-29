58eacd2c725d614c9e63eb6eb07027a2d2225c44
ChoiKYH <priyune@naver.com>
2025-11-29 12:00:17 +0900

    feat(post): update topic dropdown

--- src/main/java/com/edurican/flint/core/api/controller/v1/response/TopicResponse.java
+++ src/main/java/com/edurican/flint/core/api/controller/v1/response/TopicResponse.java
@@ -1,4 +1,20 @@
 package com.edurican.flint.core.api.controller.v1.response;
 
+import com.edurican.flint.core.domain.Topic;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class TopicResponse {
+
+    private Long id;
+    private String topicName;
+
+    public static TopicResponse from(Topic topic) {
+        return TopicResponse.builder()
+                .id(topic.getId())
+                .topicName(topic.getTopicName())
+                .build();
+    }
 }
